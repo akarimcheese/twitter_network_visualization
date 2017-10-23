66aa428e41b1b69e7a781366177478a0d8341730
Ayman Karim <akarim1@terpmail.umd.edu>
2017-10-23 05:20:58 +0000

    Comments

--- src/twitter_network_example/version.cr
+++
@@ -1,3 +0,0 @@
-module TwitterNetworkExample
-  VERSION = "0.1.0"
-end
--- src/twitter_network_visualization.cr
+++ src/twitter_network_visualization.cr
@@ -4,6 +4,7 @@ require "kemal"
 require "twitter_networks"
 
 module TwitterNetworkExample
+    # Handles communication with socket and Twitter API
     class Client
       property network : Twitter::Network
       property socket : HTTP::WebSocket?
@@ -19,25 +20,34 @@ module TwitterNetworkExample
         @socket = nil
       end
       
+      # Callback to give to Twitter::Network for when it finds a user that exists
       private def receive_user(user)
         if socket = @socket
+          # Let the browser know a user was found. Provide the image as well.
           socket.send "{\"node\":#{user.screen_name.inspect},\"img\":#{user.profile_image_url_https.inspect}}"
         end
       end
       
+      # Callback to give to Twitter::Network for when it finds a relationship
       private def receive_relationship(source, target)
         if socket = @socket
+          # Let the browser know a relationship was found.
           socket.send "{\"source\":#{source.inspect},\"target\":#{target.inspect}}"
         end
       end
       
+      # Callback to give to Twitter::Network for when its Twitter API Client gets ratelimited
       private def receive_rate_limit()
         if socket = @socket
+          # Let the browser know the rate limit was reached
           socket.send "{\"message\":\"Rate Limit Reached\"}"
         end
       end
       
+      # Register the socket for this client
       def addSocket(socket)
+        # If this network client was active for a previous socket,
+        # catch up the current socket with all the users and links
         @network.each_user do |user|
           socket.send "{\"node\":#{user.screen_name.inspect},\"img\":#{user.profile_image_url_https.inspect}}"
         end
@@ -46,16 +56,19 @@ module TwitterNetworkExample
           socket.send "{\"source\":#{source.inspect},\"target\":#{target.inspect}}"
         end
     
+        # When the socket sends a user to add, handle it asynchronously
         socket.on_message do |message|
           spawn do
             @network.add_user(message)
           end
         end
       
+        # When the socket closes, unregister it from this client
         socket.on_close do
           @socket = nil
         end
       
+        # Set callbacks for Twitter::Network
         @network.on_user_added do |user|
           receive_user(user)
         end
@@ -72,8 +85,10 @@ module TwitterNetworkExample
       end
     end
     
-    
+    # SessionId => Client. Client handles Socket and Twitter API stuff
     clients = {} of String => Client
+    
+    # Handles the OAuth stuff
     auth_client = Twitter::Auth::Client.new(
         ENV["TWITTER_CONSUMER_KEY"], 
         ENV["TWITTER_CONSUMER_SECRET"], 
@@ -85,16 +100,21 @@ module TwitterNetworkExample
     
     # Matches GET "http://host:port/"
     get "/" do
+      # oauth_request is the object containing the oauth link
       oauth_request = auth_client.oauth_request_token("#{ENV["HOSTNAME"]}/auth")
       render "views/index.ecr"
     end
     
+    # Matches GET "http://host:port/auth"
     get "/auth" do |ctx|
+      # This endpoint should be reached by the Twitter OAuth callback with the oauth token and oauth verifier
       auth_params = ctx.request.query_params
       oauth_token, oauth_verifier = auth_params["oauth_token"], auth_params["oauth_verifier"]
+      # Get the access token from the OAuth callback params
       access_token = auth_client.oauth_access_token(oauth_token, oauth_verifier)
       session_id = SecureRandom.hex
       
+      # Set a session cookie
       ctx.response.cookies["session"] = HTTP::Cookie.new("session", session_id, "/", Time.now + 12.hours, nil, true)
       clients[session_id] = Client.new(access_token)
       
@@ -103,6 +123,7 @@ module TwitterNetworkExample
 
     # Matches GET "http://host:port/network"
     get "/network" do |ctx|
+      # If there's no session, redirect to home page
       if !ctx.request.cookies["session"]?
         ctx.redirect "/"
       elsif !clients[ctx.request.cookies["session"].value]?
