3621781ad87af783f11b62594c606091775d8a31
Ayman Karim <akarim1@terpmail.umd.edu>
2017-11-19 00:05:30 +0000

    Revert "Use animation frame"
    This reverts commit da50f3129c0e27473fc34a076d9adefb3afc18d4.

--- views/js/network.js
+++ views/js/network.js
@@ -111,7 +111,7 @@ class Node {
         }
         
         // Repeat step if appearance isn't complete
-        window.requestAnimationFrame(() => {this.appearStep(svg)});
+        setTimeout(() => {this.appearStep(svg)}, 1);
     }
     
     appear(svg, img) {
@@ -206,7 +206,7 @@ class Edge {
             // Show little bit more of the path
             linkSvg.setAttribute("stroke-dashoffset", `${(1-this.surgeProgress)*100}%`);
              
-            window.requestAnimationFrame(() => { this.surgeStep(sourceSvg, targetSvg, svg); });
+            setTimeout(() => { this.surgeStep(sourceSvg, targetSvg, svg); }, 5);
         } 
     }
     
